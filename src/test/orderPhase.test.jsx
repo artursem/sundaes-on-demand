10b93e3c51acd3be421505005ddf65dcdabdcf7b
artursem <artur.semeniuk@gmail.com>
2022-02-10 20:42:17 +0100

    Test for optional toppings

--- src/test/orderPhase.test.jsx
+++ src/test/orderPhase.test.jsx
@@ -1,10 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('order phases for happy path', async () => {
 	// render App
 	render(<App />);
+
 	// add scoops & toppings
 	const vanillaInput = await screen.findByRole('spinbutton', {
 		name: 'Vanilla',
@@ -17,6 +22,7 @@ test('order phases for happy path', async () => {
 	// find and click order button
 	const orderButton = screen.getByRole('button', { name: /order sundae/i });
 	userEvent.click(orderButton);
+
 	// check summary
 	const summaryHeading = screen.getByRole('heading', { name: 'Order Summary' });
 	expect(summaryHeading).toBeInTheDocument();
@@ -26,17 +32,28 @@ test('order phases for happy path', async () => {
 	expect(toppingsHeading).toBeInTheDocument();
 	const termsAndConditions = await screen.findByRole('checkbox');
 	userEvent.click(termsAndConditions);
+
 	// accept terms and conditions and confirm
 	const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
 	userEvent.click(confirmButton);
+
+	//Loading
+	const loading = screen.getByText('Loading');
+	expect(loading).toBeInTheDocument();
+	await waitForElementToBeRemoved(() => screen.queryByText('Loading'));
+
 	// confirm order number
-	const orderNumber = await screen.findByText(/order number/i);
+	const thankYou = await screen.findByRole('heading', { name: 'Thank You!' });
+	expect(thankYou).toBeInTheDocument();
+	const orderNumber = screen.getByText(/order number/i);
 	expect(orderNumber).toBeInTheDocument();
+
 	// click new order button
 	const createOrderButton = await screen.findByRole('button', {
 		name: 'Create new order',
 	});
 	userEvent.click(createOrderButton);
+
 	// check if orders are reset
 	const vanillaInputReset = await screen.findByRole('spinbutton', {
 		name: 'Vanilla',
@@ -44,4 +61,31 @@ test('order phases for happy path', async () => {
 	expect(vanillaInputReset).toHaveValue(0);
 	const grandTotal = screen.getByRole('heading', { name: /grand total: \$/i });
 	expect(grandTotal).toHaveTextContent('0.00');
+});
+
+test('optional toppings on summary page', async () => {
+	// render App
+	render(<App />);
+
+	// add scoops
+	const vanillaInput = await screen.findByRole('spinbutton', {
+		name: 'Vanilla',
+	});
+	userEvent.clear(vanillaInput);
+	userEvent.type(vanillaInput, '1');
+
+	// find and click order button
+	const orderButton = screen.getByRole('button', { name: /order sundae/i });
+	userEvent.click(orderButton);
+
+	// check summary
+	const summaryHeading = screen.getByRole('heading', { name: 'Order Summary' });
+	expect(summaryHeading).toBeInTheDocument();
+	const scoopsHeading = screen.getByRole('heading', { name: /scoops/i });
+	expect(scoopsHeading).toBeInTheDocument();
+
+	const toppingsHeadingNot = screen.queryByRole('heading', {
+		name: /toppings/i,
+	});
+	expect(toppingsHeadingNot).not.toBeInTheDocument();
 });
